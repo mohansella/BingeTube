38db2b73d1b679835605d1ba16a2187c5ba9c284
mohansella <rkssmohankumar@gmail.com>
2025-11-28 08:13:25 +0530

    search channel,video tab skeleton

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -2,43 +2,76 @@ import 'package:bingetube/core/config/configuration.dart';
 import 'package:bingetube/core/log/log_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:logging/logging.dart';
 
 class SearchPage extends ConsumerStatefulWidget {
+  static final Logger logger = LogManager.getLogger('SearchPage');
+
   const SearchPage({super.key});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() => SearchPageState();
 }
 
-class SearchPageState extends ConsumerState<SearchPage> {
-  bool _isConfigured = false;
+class SearchPageState extends ConsumerState<SearchPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  late TextEditingController _textController;
+
+  String? _searchQuery;
 
   @override
   void initState() {
     super.initState();
-    _isConfigured = ref.read(ConfigProviders.apiKeyMeta).apiKey.isNotEmpty;
+    _tabController = TabController(
+      length: 2,
+      vsync: this,
+      animationDuration: Duration(milliseconds: 250),
+    );
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    bool isKeyConfigured = ref
+        .watch(ConfigProviders.apiKeyMeta)
+        .apiKey
+        .isNotEmpty;
     return Scaffold(
       appBar: AppBar(
         title: SizedBox(
           height: 40,
           child: TextField(
-            enabled: _isConfigured,
+            autofocus: true,
+            enabled: isKeyConfigured,
+            controller: _textController,
             decoration: InputDecoration(
               hintText: 'Search...',
               suffixIcon: Icon(Icons.search),
             ),
+            onTapOutside: (event) {
+              _textController.text = _searchQuery ?? '';
+              FocusScope.of(context).unfocus();
+            },
+            onSubmitted: (query) {
+              setState(() {
+                _searchQuery = query;
+              });
+            },
           ),
         ),
       ),
-      body: DefaultTabController(
-        length: 2,
-        child: Column(
-          children: [
+      body: Column(
+        children: [
+          if (isKeyConfigured) ...[
             TabBar(
+              controller: _tabController,
               tabs: [
                 Tab(text: 'Channels'),
                 Tab(text: 'Videos'),
@@ -46,15 +79,67 @@ class SearchPageState extends ConsumerState<SearchPage> {
             ),
             Expanded(
               child: TabBarView(
+                controller: _tabController,
                 children: [
-                  SingleChildScrollView(child: const Text('Channels result')),
-                  SingleChildScrollView(child: const Text('Videos result')),
+                  _SearchChannelPage(query: _searchQuery),
+                  _SearchVideoPage(query: _searchQuery),
                 ],
               ),
             ),
+          ] else ...[
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: const Text(
+                  'Add your YouTube API key to start searching',
+                ),
+              ),
+            ),
           ],
-        ),
+        ],
       ),
     );
+  }
+}
+
+class _SearchChannelPage extends ConsumerStatefulWidget {
+  final String? query;
+
+  const _SearchChannelPage({required this.query});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _SearchChannelPageState();
+}
+
+class _SearchChannelPageState extends ConsumerState<_SearchChannelPage> {
+  @override
+  Widget build(BuildContext context) {
+    if(widget.query?.isEmpty ?? true) {
+      return Text('search channel');
+    } else {
+      return Text('search channel for result: ${widget.query}');
+    }
+  }
+}
+
+class _SearchVideoPage extends ConsumerStatefulWidget {
+  final String? query;
+
+  const _SearchVideoPage({required this.query});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _SearchVideoPageState();
+}
+
+class _SearchVideoPageState extends ConsumerState<_SearchVideoPage> {
+  @override
+  Widget build(BuildContext context) {
+    if(widget.query?.isEmpty ?? true) {
+      return Text('search video');
+    } else {
+      return Text('search video for result: ${widget.query}');
+    }
   }
 }
