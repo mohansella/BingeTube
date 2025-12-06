c343d4995caa7d31aad629e408ce0d03fbb54187
mohansella <45608236+mohansella@users.noreply.github.com>
2025-12-06 22:24:50 +0530

    code generation in generated folder

--- lib/generated/core/db/database.g.dart
+++ lib/generated/core/db/database.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'database.dart';
+part of '../../../core/db/database.dart';
 
 // ignore_for_file: type=lint
 class $VideoMetaTable extends VideoMeta
