bc22421d8a093252f958b6ed8bc56938690bcbfd
mohansella <45608236+mohansella@users.noreply.github.com>
2025-12-11 20:28:13 +0530

    search channel from cache table

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,3 @@
+sealed class PageSizeConstants {
+  static final channelEntriesInSearchPage = 50;
+}
---
+++ lib/generated/core/db/access/search.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of '../../../../core/db/access/search.dart';
+
+// ignore_for_file: type=lint
+mixin _$SearchDaoMixin on DatabaseAccessor<Database> {
+  $ChannelSearchesTable get channelSearches => attachedDatabase.channelSearches;
+  $ChannelsTable get channels => attachedDatabase.channels;
+  $ChannelSearchVsChannelsTable get channelSearchVsChannels =>
+      attachedDatabase.channelSearchVsChannels;
+}
