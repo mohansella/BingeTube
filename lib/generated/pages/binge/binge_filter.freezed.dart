e00f4868a15e08200fd8277cce48d140346a233f
mohansella <45608236+mohansella@users.noreply.github.com>
2025-12-26 19:09:35 +0530

    filter and widget separated

--- lib/generated/pages/binge/binge_filter.freezed.dart
+++ lib/generated/pages/binge/binge_filter.freezed.dart
@@ -14,7 +14,7 @@ T _$identity<T>(T value) => value;
 /// @nodoc
 mixin _$BingeFilter {
 
- BingeFilterWatchType get watchType; BingeFilterSortOrder get sortOrder; BingeFilterSortType get sortType; String? get searchValue;
+ BingeFilterWatchType get watchType; BingeFilterSortOrder get sortOrder; BingeFilterSortType get sortType; DateTime? get fromRange; DateTime? get toRange; String? get searchValue;
 /// Create a copy of BingeFilter
 /// with the given fields replaced by the non-null parameter values.
 @JsonKey(includeFromJson: false, includeToJson: false)
@@ -25,16 +25,16 @@ $BingeFilterCopyWith<BingeFilter> get copyWith => _$BingeFilterCopyWithImpl<Bing
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is BingeFilter&&(identical(other.watchType, watchType) || other.watchType == watchType)&&(identical(other.sortOrder, sortOrder) || other.sortOrder == sortOrder)&&(identical(other.sortType, sortType) || other.sortType == sortType)&&(identical(other.searchValue, searchValue) || other.searchValue == searchValue));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is BingeFilter&&(identical(other.watchType, watchType) || other.watchType == watchType)&&(identical(other.sortOrder, sortOrder) || other.sortOrder == sortOrder)&&(identical(other.sortType, sortType) || other.sortType == sortType)&&(identical(other.fromRange, fromRange) || other.fromRange == fromRange)&&(identical(other.toRange, toRange) || other.toRange == toRange)&&(identical(other.searchValue, searchValue) || other.searchValue == searchValue));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,watchType,sortOrder,sortType,searchValue);
+int get hashCode => Object.hash(runtimeType,watchType,sortOrder,sortType,fromRange,toRange,searchValue);
 
 @override
 String toString() {
-  return 'BingeFilter(watchType: $watchType, sortOrder: $sortOrder, sortType: $sortType, searchValue: $searchValue)';
+  return 'BingeFilter(watchType: $watchType, sortOrder: $sortOrder, sortType: $sortType, fromRange: $fromRange, toRange: $toRange, searchValue: $searchValue)';
 }
 
 
@@ -45,7 +45,7 @@ abstract mixin class $BingeFilterCopyWith<$Res>  {
   factory $BingeFilterCopyWith(BingeFilter value, $Res Function(BingeFilter) _then) = _$BingeFilterCopyWithImpl;
 @useResult
 $Res call({
- BingeFilterWatchType watchType, BingeFilterSortOrder sortOrder, BingeFilterSortType sortType, String? searchValue
+ BingeFilterWatchType watchType, BingeFilterSortOrder sortOrder, BingeFilterSortType sortType, DateTime? fromRange, DateTime? toRange, String? searchValue
 });
 
 
@@ -62,12 +62,14 @@ class _$BingeFilterCopyWithImpl<$Res>
 
 /// Create a copy of BingeFilter
 /// with the given fields replaced by the non-null parameter values.
-@pragma('vm:prefer-inline') @override $Res call({Object? watchType = null,Object? sortOrder = null,Object? sortType = null,Object? searchValue = freezed,}) {
+@pragma('vm:prefer-inline') @override $Res call({Object? watchType = null,Object? sortOrder = null,Object? sortType = null,Object? fromRange = freezed,Object? toRange = freezed,Object? searchValue = freezed,}) {
   return _then(_self.copyWith(
 watchType: null == watchType ? _self.watchType : watchType // ignore: cast_nullable_to_non_nullable
 as BingeFilterWatchType,sortOrder: null == sortOrder ? _self.sortOrder : sortOrder // ignore: cast_nullable_to_non_nullable
 as BingeFilterSortOrder,sortType: null == sortType ? _self.sortType : sortType // ignore: cast_nullable_to_non_nullable
-as BingeFilterSortType,searchValue: freezed == searchValue ? _self.searchValue : searchValue // ignore: cast_nullable_to_non_nullable
+as BingeFilterSortType,fromRange: freezed == fromRange ? _self.fromRange : fromRange // ignore: cast_nullable_to_non_nullable
+as DateTime?,toRange: freezed == toRange ? _self.toRange : toRange // ignore: cast_nullable_to_non_nullable
+as DateTime?,searchValue: freezed == searchValue ? _self.searchValue : searchValue // ignore: cast_nullable_to_non_nullable
 as String?,
   ));
 }
@@ -153,10 +155,10 @@ return $default(_that);case _:
 /// }
 /// ```
 
-@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  String? searchValue)?  $default,{required TResult orElse(),}) {final _that = this;
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>(TResult Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  DateTime? fromRange,  DateTime? toRange,  String? searchValue)?  $default,{required TResult orElse(),}) {final _that = this;
 switch (_that) {
 case _BingeFilter() when $default != null:
-return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue);case _:
+return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.fromRange,_that.toRange,_that.searchValue);case _:
   return orElse();
 
 }
@@ -174,10 +176,10 @@ return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue
 /// }
 /// ```
 
-@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  String? searchValue)  $default,) {final _that = this;
+@optionalTypeArgs TResult when<TResult extends Object?>(TResult Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  DateTime? fromRange,  DateTime? toRange,  String? searchValue)  $default,) {final _that = this;
 switch (_that) {
 case _BingeFilter():
-return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue);case _:
+return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.fromRange,_that.toRange,_that.searchValue);case _:
   throw StateError('Unexpected subclass');
 
 }
@@ -194,10 +196,10 @@ return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue
 /// }
 /// ```
 
-@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  String? searchValue)?  $default,) {final _that = this;
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>(TResult? Function( BingeFilterWatchType watchType,  BingeFilterSortOrder sortOrder,  BingeFilterSortType sortType,  DateTime? fromRange,  DateTime? toRange,  String? searchValue)?  $default,) {final _that = this;
 switch (_that) {
 case _BingeFilter() when $default != null:
-return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue);case _:
+return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.fromRange,_that.toRange,_that.searchValue);case _:
   return null;
 
 }
@@ -209,12 +211,14 @@ return $default(_that.watchType,_that.sortOrder,_that.sortType,_that.searchValue
 
 
 class _BingeFilter implements BingeFilter {
-  const _BingeFilter({required this.watchType, required this.sortOrder, required this.sortType, required this.searchValue});
+  const _BingeFilter({required this.watchType, required this.sortOrder, required this.sortType, required this.fromRange, required this.toRange, required this.searchValue});
   
 
 @override final  BingeFilterWatchType watchType;
 @override final  BingeFilterSortOrder sortOrder;
 @override final  BingeFilterSortType sortType;
+@override final  DateTime? fromRange;
+@override final  DateTime? toRange;
 @override final  String? searchValue;
 
 /// Create a copy of BingeFilter
@@ -227,16 +231,16 @@ _$BingeFilterCopyWith<_BingeFilter> get copyWith => __$BingeFilterCopyWithImpl<_
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is _BingeFilter&&(identical(other.watchType, watchType) || other.watchType == watchType)&&(identical(other.sortOrder, sortOrder) || other.sortOrder == sortOrder)&&(identical(other.sortType, sortType) || other.sortType == sortType)&&(identical(other.searchValue, searchValue) || other.searchValue == searchValue));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _BingeFilter&&(identical(other.watchType, watchType) || other.watchType == watchType)&&(identical(other.sortOrder, sortOrder) || other.sortOrder == sortOrder)&&(identical(other.sortType, sortType) || other.sortType == sortType)&&(identical(other.fromRange, fromRange) || other.fromRange == fromRange)&&(identical(other.toRange, toRange) || other.toRange == toRange)&&(identical(other.searchValue, searchValue) || other.searchValue == searchValue));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,watchType,sortOrder,sortType,searchValue);
+int get hashCode => Object.hash(runtimeType,watchType,sortOrder,sortType,fromRange,toRange,searchValue);
 
 @override
 String toString() {
-  return 'BingeFilter(watchType: $watchType, sortOrder: $sortOrder, sortType: $sortType, searchValue: $searchValue)';
+  return 'BingeFilter(watchType: $watchType, sortOrder: $sortOrder, sortType: $sortType, fromRange: $fromRange, toRange: $toRange, searchValue: $searchValue)';
 }
 
 
@@ -247,7 +251,7 @@ abstract mixin class _$BingeFilterCopyWith<$Res> implements $BingeFilterCopyWith
   factory _$BingeFilterCopyWith(_BingeFilter value, $Res Function(_BingeFilter) _then) = __$BingeFilterCopyWithImpl;
 @override @useResult
 $Res call({
- BingeFilterWatchType watchType, BingeFilterSortOrder sortOrder, BingeFilterSortType sortType, String? searchValue
+ BingeFilterWatchType watchType, BingeFilterSortOrder sortOrder, BingeFilterSortType sortType, DateTime? fromRange, DateTime? toRange, String? searchValue
 });
 
 
@@ -264,12 +268,14 @@ class __$BingeFilterCopyWithImpl<$Res>
 
 /// Create a copy of BingeFilter
 /// with the given fields replaced by the non-null parameter values.
-@override @pragma('vm:prefer-inline') $Res call({Object? watchType = null,Object? sortOrder = null,Object? sortType = null,Object? searchValue = freezed,}) {
+@override @pragma('vm:prefer-inline') $Res call({Object? watchType = null,Object? sortOrder = null,Object? sortType = null,Object? fromRange = freezed,Object? toRange = freezed,Object? searchValue = freezed,}) {
   return _then(_BingeFilter(
 watchType: null == watchType ? _self.watchType : watchType // ignore: cast_nullable_to_non_nullable
 as BingeFilterWatchType,sortOrder: null == sortOrder ? _self.sortOrder : sortOrder // ignore: cast_nullable_to_non_nullable
 as BingeFilterSortOrder,sortType: null == sortType ? _self.sortType : sortType // ignore: cast_nullable_to_non_nullable
-as BingeFilterSortType,searchValue: freezed == searchValue ? _self.searchValue : searchValue // ignore: cast_nullable_to_non_nullable
+as BingeFilterSortType,fromRange: freezed == fromRange ? _self.fromRange : fromRange // ignore: cast_nullable_to_non_nullable
+as DateTime?,toRange: freezed == toRange ? _self.toRange : toRange // ignore: cast_nullable_to_non_nullable
+as DateTime?,searchValue: freezed == searchValue ? _self.searchValue : searchValue // ignore: cast_nullable_to_non_nullable
 as String?,
   ));
 }
