05a4c57df773979f5e461cbd16e9efe19d5c1bc5
mohansella <rkssmohankumar@gmail.com>
2026-01-10 00:58:56 +0530

    init analytics at app start to observe route changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bingetube/app/app.dart';
+import 'package:bingetube/core/analytics/analytics.dart';
 import 'package:bingetube/core/config/configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -6,5 +7,6 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ConfigStore.init();
+  await Analytics.init();
   runApp(ProviderScope(child: const App()));
 }
