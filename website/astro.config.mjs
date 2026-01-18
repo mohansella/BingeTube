733f6edfbfa8450045d6900002bc29d62996996f
mohansella <45608236+mohansella@users.noreply.github.com>
2026-01-18 17:54:51 +0530

    tailwind added. website generated.

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://bingetube.io',
+
   integrations: [
     sitemap(),
   ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
