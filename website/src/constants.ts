1ba1ea236d6098d5bc5048829a590b9ad2ca2709
mohansella <45608236+mohansella@users.noreply.github.com>
2026-01-20 21:14:14 +0530

    constants of site values from ts file

---
+++ website/src/constants.ts
@@ -0,0 +1,6 @@
+export class SiteContants {
+  static readonly APP_NAME = 'BingeTube'
+  static readonly DOMAIN = 'https://bingetube.io'
+  static readonly APP_URL = '${DOMAIN}/app'
+  static readonly SUPPORT_MAIL = 'mohansella.store@gmail.com'
+}
