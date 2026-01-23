1137d941610df56b970b92fb172208db0d462003
mohansella <45608236+mohansella@users.noreply.github.com>
2026-01-23 21:35:40 +0530

    fix: open app in website url

--- website/src/constants.ts
+++ website/src/constants.ts
@@ -1,6 +1,6 @@
 export class SiteContants {
   static readonly APP_NAME = 'BingeTube'
-  static readonly DOMAIN = 'https://bingetube.io'
+  static readonly DOMAIN = ''
   static readonly APP_URL = `${SiteContants.DOMAIN}/app`
   static readonly SUPPORT_MAIL = 'mohansella.store@gmail.com'
 }
