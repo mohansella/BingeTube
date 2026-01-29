4a6c533bea55b1b7f28fefbaf7d3f46ae9c1a1d3
mohansella <45608236+mohansella@users.noreply.github.com>
2026-01-29 11:09:58 +0530

    basic channel info icon added

--- lib/pages/channel/channel_page.dart
+++ lib/pages/channel/channel_page.dart
@@ -4,6 +4,7 @@ import 'package:bingetube/core/db/database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:readmore/readmore.dart';
 
 enum _Params { channelId, heroId, heroImg }
 
@@ -37,6 +38,7 @@ class _ChannelPageState extends ConsumerState<ChannelPage> {
 
   bool isModelLoading = true;
   late ChannelModel model;
+  late double width;
 
   @override
   void initState() {
@@ -58,19 +60,71 @@ class _ChannelPageState extends ConsumerState<ChannelPage> {
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
+        width = constraints.minWidth;
         return SafeArea(
           child: Scaffold(
             appBar: AppBar(),
-            body: Hero(
-              tag: heroId,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(80),
-                child: _buildVideoCardImage(heroImg, heroId),
-              ),
+            body: SingleChildScrollView(
+              child: Column(children: [_buildChannelInfo()]),
             ),
           ),
         );
       },
+    );
+  }
+
+  Widget _buildChannelInfo() {
+    final theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0),
+      child: Column(
+        crossAxisAlignment: .start,
+        children: [
+          Row(
+            mainAxisSize: .min,
+            children: [
+              Hero(
+                tag: heroId,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(100),
+                  child: _buildVideoCardImage(heroImg, heroId),
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Column(
+                crossAxisAlignment: .start,
+                children: [
+                  if (!isModelLoading) ...[
+                    Text(
+                      model.snippet.title,
+                      style: theme.textTheme.titleLarge,
+                    ),
+                    Text(
+                      _buildSubsAndVideosText(),
+                      style: theme.textTheme.labelMedium?.copyWith(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ],
+              ),
+            ],
+          ),
+          if (!isModelLoading) ...[
+            const SizedBox(height: 8.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ReadMoreText(
+                model.snippet.description,
+                style: theme.textTheme.bodySmall,
+                trimLines: 2,
+                trimMode: .Line,
+                
+              ),
+            ),
+          ],
+        ],
+      ),
     );
   }
 
@@ -93,5 +147,28 @@ class _ChannelPageState extends ConsumerState<ChannelPage> {
     final brightness = theme.brightness;
     final color = Themes.colorFromId(id, brightness);
     return Container(color: color, alignment: .center);
+  }
+
+  String _buildSubsAndVideosText() {
+    final buffer = StringBuffer();
+    if (!model.statistics.hiddenSubscriberCount) {
+      final count = model.statistics.subscriberCount;
+      _writeShortNumber(count, buffer);
+      buffer.write(' subscribers * ');
+    }
+    _writeShortNumber(model.statistics.videoCount, buffer);
+    buffer.write(' videos');
+
+    return buffer.toString();
+  }
+
+  void _writeShortNumber(int count, StringBuffer buffer) {
+    if (count >= 1000000) {
+      buffer.write('${(count / 1000000).toStringAsFixed(1)}M');
+    } else if (count >= 1000) {
+      buffer.write('${(count / 1000).toStringAsFixed(1)}K');
+    } else {
+      buffer.write(count.toString());
+    }
   }
 }
