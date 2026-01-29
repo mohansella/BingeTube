a7fbef26a0cf093a5192933ad704cb7ae33d208f
mohansella <45608236+mohansella@users.noreply.github.com>
2026-01-29 21:50:06 +0530

    playlist schema created

---
+++ lib/core/db/tables/playlists.dart
@@ -0,0 +1,44 @@
+import 'package:bingetube/core/db/tables/channels.dart';
+import 'package:bingetube/core/db/tables/mixins.dart';
+import 'package:drift/drift.dart';
+
+@TableIndex(name: 'PlaylistIndexETag', columns: {#etag})
+class Playlists extends Table with TableTimedMixin {
+  late final id = text()();
+  late final etag = text().nullable()();
+  late final channelId = text().references(Channels, #id)();
+
+  @override
+  get primaryKey => {id};
+}
+
+class PlaylistSnippets extends Table with TableTimedMixin {
+  late final id = text().references(Playlists, #id)();
+  late final publishedAt = dateTime()();
+  late final title = text()();
+  late final description = text()();
+  late final channelTitle = text()();
+
+  @override
+  get primaryKey => {id};
+}
+
+class PlaylistThumbnails extends Table with TableTimedMixin {
+  late final id = text().references(Playlists, #id)();
+  late final defaultUrl = text()();
+  late final mediumUrl = text()();
+  late final highUrl = text()();
+  late final standardUrl = text().nullable()();
+  late final maxresUrl = text().nullable()();
+
+  @override
+  get primaryKey => {id};
+}
+
+class PlaylistContentDetails extends Table with TableTimedMixin {
+  late final id = text().references(Playlists, #id)();
+  late final itemCount = integer()();
+
+  @override
+  get primaryKey => {id};
+}
