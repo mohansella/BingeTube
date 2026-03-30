4f18e084670335f34ffa2acdf55098e16d8d292d
mohansella <45608236+mohansella@users.noreply.github.com>
2026-03-30 10:36:24 +0530

    using url instead of hash path in url

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:bingetube/core/analytics/analytics.dart';
 import 'package:bingetube/core/config/configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
   await ConfigStore.init();
   await Analytics.init();
   runApp(ProviderScope(child: const App()));
