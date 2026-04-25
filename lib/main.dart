29ace95d3ef3d4bc97ce7c932d2e3281da2d34fe
mohansella <45608236+mohansella@users.noreply.github.com>
2026-04-25 10:39:54 +0530

    updating redirects logic and changing to url path instead of fragment

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:bingetube/core/analytics/analytics.dart';
 import 'package:bingetube/core/config/configuration.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  usePathUrlStrategy();
   await ConfigStore.init();
   await Analytics.init();
   runApp(ProviderScope(child: const App()));
