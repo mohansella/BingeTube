ce1b61bf836ef3e578852fda790bbb659e5141ab
mohansella <45608236+mohansella@users.noreply.github.com>
2026-05-05 20:59:59 +0530

    removed dpad dependency to handle tv later

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,7 +1,6 @@
 import 'package:bingetube/app/routes.dart';
 import 'package:bingetube/app/theme.dart';
 import 'package:bingetube/core/config/configuration.dart';
-import 'package:dpad/dpad.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,16 +12,13 @@ class App extends ConsumerWidget {
     final themeMode = ref.watch(ConfigProviders.theme);
     final appFontSize = ref.watch(ConfigProviders.appFontSize);
     final routerConfig = Routes.getRouterConfig();
-    return DpadNavigator(
-      onBackPressed: () => Routes.onBackPressed(context),
-      child: MaterialApp.router(
-        themeMode: themeMode,
-        theme: Themes.light(appFontSize),
-        darkTheme: Themes.dark(appFontSize),
-        routerConfig: routerConfig,
-        scaffoldMessengerKey: Routes.getScaffoldMessengerKey(),
-        debugShowCheckedModeBanner: true,
-      ),
+    return MaterialApp.router(
+      themeMode: themeMode,
+      theme: Themes.light(appFontSize),
+      darkTheme: Themes.dark(appFontSize),
+      routerConfig: routerConfig,
+      scaffoldMessengerKey: Routes.getScaffoldMessengerKey(),
+      debugShowCheckedModeBanner: true,
     );
   }
 }
--- lib/common/widget/binge/list_screen.dart
+++ lib/common/widget/binge/list_screen.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:bingetube/common/widget/custom_dialog.dart';
 import 'package:bingetube/core/db/repo/series_repo.dart';
 import 'package:bingetube/core/lang/mutable.dart';
-import 'package:dpad/dpad.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -297,15 +296,12 @@ class _ListScreenWidgetState extends State<ListScreenWidget>
         return item;
       },
       child: DraggableWidget(
-        child: DpadFocusable(
-          onSelect: onTap,
-          child: Material(
-            child: InkWell(
-              onTap: onTap,
-              child: Transform.translate(
-                offset: Offset(dropShift, 0.0),
-                child: _buildActualSery(heroId, heroImg, model),
-              ),
+        child: Material(
+          child: InkWell(
+            onTap: onTap,
+            child: Transform.translate(
+              offset: Offset(dropShift, 0.0),
+              child: _buildActualSery(heroId, heroImg, model),
             ),
           ),
         ),
